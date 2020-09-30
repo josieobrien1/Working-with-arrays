1054946066f0cce61ba5f8484d5078f0767e755f
josieobrien1 <70530151+josieobrien1@users.noreply.github.com>
2020-09-29 20:05:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+var wrapperEle = document.body.querySelector(".wrapper");
+// var list=["Bob","Jerry","Mavis","Morty","Porty","Larry"];
+ var warriors = [
+  {
+    name:"Bob",
+    damage:2,
+    health:10,
+    warrior:true
+  },
+  {
+    name:"Jerry",
+    damage:1,
+    health:12,
+    warrior:true
+  },
+  {
+    name:"Mavis",
+    damage:2,
+    health:10,
+    warrior:true
+  },
+  {
+    name:"Morty",
+    damage:4,
+    health:10,
+    warrior:true
+  },
+  {
+    name:"Shorty",
+    damage:10,
+    health:3,
+    warrior:false
+  },
+  {
+    name:"Porty",
+    damage:1,
+    health:14,
+    warrior:true
+  },{
+    name:"Perry",
+    damage:2,
+    health:9,
+    warrior:true
+  },
+  {
+    name:"Larry",
+    damage:2,
+    health:17,
+    warrior:false
+  }
+];
+ for(var i=0; i<warriors.length; i++){
+  if(warriors[i].warrior&&warriors[i].health>=10&&warriors[i].damage>=2)
+   {
+     
+    var personEle = document.createElement("div");
+  personEle.innerHTML=warriors[i].name;
+     if(warriors[i].name.includes("a"))
+     {
+       personEle.style.color = "red";
+     }
+  wrapperEle.appendChild(personEle);
+  }                    
+}
